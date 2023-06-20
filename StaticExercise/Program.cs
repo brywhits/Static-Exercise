ef84a444ea82e24d250d37629fdcd60aa4be1ea2
bryantwhitsett <bryantwhitsett71@gmail.com>
2023-06-19 23:18:35 -0400

    Static

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.FahrenheitToCelsius(20);
+
+            Console.WriteLine($"Celcius is: {celsius}!");
+            Console.WriteLine($"Fahrenheit is: {fahrenheit}!");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		public static double FahrenheitToCelsius(double fahrenheit)
+
+        {
+			var result = (fahrenheit - 32) / 1.8;
+
+			return result;
+		}
+
+		public static double CelsiusToFahreheit(double celsius)
+		{
+			return (celsius * 9) / 5 + 32;
+		}
+
+    }
+}
+
